4ce95453ee00ecca8085b7d54845fefa33bec3fb
Bruno Morais <brunosmmm@gmail.com>
2020-01-12 12:02:46 -0500

    generate actual configuration file

--- cfggen.py
+++ cfggen.py
@@ -9,6 +9,9 @@ if __name__ == "__main__":
 
     parser = ArgumentParser()
     parser.add_argument("config", help="configuration file")
+    parser.add_argument(
+        "--output", help="configuration output", default="config.txt"
+    )
 
     args = parser.parse_args()
 
@@ -24,4 +27,18 @@ if __name__ == "__main__":
         exit(1)
     except TestConfigurationError as ex:
         print(f"ERROR: configuration error: '{ex}'")
+        exit(1)
+
+    # generate configuration
+    trigger_stages = config["trigger_config"]
+    try:
+        with open(args.output, "w") as config_out:
+            for stage in trigger_stages:
+                config_out.write("{}\n".format(hex(stage["mask"])))
+                config_out.write("{}\n".format(hex(stage["type"])))
+                config_out.write("{}\n".format(hex(stage["level"])))
+            # write size
+            config_out.write("{}\n".format(hex(config["trigger_pos"])))
+    except OSError:
+        print("ERROR: cannot write configuration file")
         exit(1)
