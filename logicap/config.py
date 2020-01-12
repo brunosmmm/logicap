29d1b58a0f230e7cb24f5e58ceff01e2d3e55a8f
Bruno Morais <brunosmmm@gmail.com>
2020-01-12 16:31:59 -0500

    update parser

--- logicap/config.py
+++ logicap/config.py
@@ -19,7 +19,7 @@ class DeferValidation(Exception):
         return self._depends
 
 
-def validate_config(config, required_keys, optional_keys):
+def validate_config(config, required_keys, optional_keys=None):
     """Validate configuration."""
     if not isinstance(config, dict):
         raise TypeError("configuration must be a dictionary")
@@ -33,21 +33,33 @@ def validate_config(config, required_keys, optional_keys):
     transformed_config = {}
     deferred_keys = {}
     for key, value in config.items():
-        if key not in required_keys and key not in optional_keys:
+        if key not in required_keys and (
+            (optional_keys is not None and key not in optional_keys)
+            or optional_keys is None
+        ):
             # warning, unknown key
+            print(f"DEBUG: unknown key '{key}'")
             continue
 
         if key in required_keys:
-            # call validate
-            if required_keys[key] is None:
-                # no validation
-                transform = None
-            else:
-                try:
-                    transform = required_keys[key](value, **transformed_config)
-                except DeferValidation as defer:
-                    deferred_keys[key] = defer.depends
-                    continue
+            key_loc = required_keys
+        elif key in optional_keys:
+            key_loc = optional_keys
+
+        # call validate
+        failed = False
+        if key_loc[key] is None:
+            # no validation
+            transform = None
+        else:
+            try:
+                transform = key_loc[key](value, **transformed_config)
+            except DeferValidation as defer:
+                deferred_keys[key] = defer.depends
+                failed = True
+
+        if failed:
+            continue
 
         if transform is not None:
             transformed_config[key] = transform
