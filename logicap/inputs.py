4e87da47932a3e305e317d573dd6ba39f7d7ddd3
Bruno Morais <brunosmmm@gmail.com>
2020-01-12 16:32:06 -0500

    initial try to implement input generator

--- logicap/inputs.py
+++ logicap/inputs.py
@@ -1,8 +1,80 @@
 """Input generation."""
 
-from logicap.config import validate_config, TestConfigurationError
+from logicap.config import (
+    validate_config,
+    TestConfigurationError,
+    DeferValidation,
+)
 
-INPUT_REQ = ()
+EVENT_TYPES = ("initial", "set", "clear", "toggle")
+_EVENT_DEPS = {
+    "initial": ("value"),
+    "set": ("mask"),
+    "clear": ("mask"),
+    "toggle": ("mask"),
+}
+
+
+def _validate_evt_type(evt_type, **kwargs):
+    """Validate event type."""
+    if not isinstance(evt_type, str):
+        raise TestConfigurationError("event type must be a string")
+
+    if evt_type not in EVENT_TYPES:
+        raise TestConfigurationError(f"unknown event type: '{evt_type}'")
+
+    evt_deps = _EVENT_DEPS[evt_type]
+    missing_deps = []
+    for dep in evt_deps:
+        if dep not in kwargs:
+            missing_deps.append(dep)
+
+        raise DeferValidation(*missing_deps)
+
+    # TODO fail if there are pending defers at end of validation
+
+
+def _validate_mask(mask, **kwargs):
+    """Validate mask."""
+
+
+def _validate_time(time, **kwargs):
+    """Validate time."""
+
+
+def _validate_value(value, **kwargs):
+    """Validate value."""
+
+
+EVENT_REQ = {"event": _validate_evt_type}
+EVENT_OPT = {
+    "mask": _validate_mask,
+    "time": _validate_time,
+    "value": _validate_value,
+}
+
+
+def _validate_event(evt_data):
+    """Validate event."""
+
+
+def _validate_sequence(seq_data, **kwargs):
+    """Validate sequence."""
+    if not isinstance(seq_data, (tuple, list)):
+        raise TestConfigurationError("sequence must be a list")
+
+    sequence = {}
+    deferred = {}
+    for idx, evt in enumerate(seq_data):
+        _evt = validate_config(evt, EVENT_REQ, EVENT_OPT)
+        sequence[idx] = _evt
+
+    print(sequence)
+
+    return sequence
+
+
+INPUT_REQ = {"sequence": _validate_sequence}
 INPUT_OPT = ()
 
 
