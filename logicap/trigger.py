1c7fd9f1298fed002cfa1b2990f13b52549d65e8
Bruno Morais <brunosmmm@gmail.com>
2020-01-13 15:01:16 -0500

    use validator framework for trigger configuration

--- logicap/trigger.py
+++ logicap/trigger.py
@@ -1,65 +1,40 @@
 """Trigger configuration."""
 
-from logicap.config import validate_config, TestConfigurationError
+from logicap.config import validate_config
 
 from logicap.validators import validate_int_percent
-from logicap.util import default_validate_pos_int, KeyDependency
+from logicap.util import (
+    default_validate_pos_int,
+    default_validate_int,
+    KeyDependency,
+    AutoValidateList,
+)
+
+TRIGGER_TYPES = ("edge", "level")
 
 
+@AutoValidateList(
+    {
+        "mask": default_validate_int,
+        "type": default_validate_int,
+        "level": default_validate_int,
+    }
+)
 def _validate_trigger_config(trigger_config, **kwargs):
     """Validate trigger configuration."""
     _TRIGGER_KEYS = ("type", "level", "mask")
-    if not isinstance(trigger_config, (tuple, list)):
-        raise TestConfigurationError("trigger configuration must be a list")
 
-    result = []
-    for idx, stage in enumerate(trigger_config):
-        if idx > 7:
-            # ignore, only 8 stages possible
-            break
-        if not isinstance(stage, dict):
-            raise TestConfigurationError(
-                "trigger stage configuration must be a dictionary"
-            )
-        _stage = {}
-        for key in _TRIGGER_KEYS:
-            if key not in stage:
-                raise TestConfigurationError(f"missing required key '{key}'")
-
-            if isinstance(stage[key], int):
-                if stage[key] < 0:
-                    raise TestConfigurationError(
-                        f"{key} must be a positive integer"
-                    )
-                key_value = stage[key]
-            elif isinstance(stage[key], str):
-                try:
-                    key_value = int(stage[key])
-                except ValueError:
-                    try:
-                        key_value = int(stage[key].lstrip("0x"), 16)
-                    except ValueError:
-                        try:
-                            key_value = int(stage[key].lstrip("0b"), 2)
-                        except ValueError:
-                            raise TestConfigurationError(
-                                f"invalid value for {key}"
-                            )
-            else:
-                raise TestConfigurationError(
-                    f"{key} must be either an integer or string"
-                )
-            _stage[key] = key_value
-        result.append(stage)
-
-    if idx < 7:
+    if len(trigger_config) < 8:
         # missing a few stages, insert blanks
         empty_stages = [
-            {key: 0 for key in _TRIGGER_KEYS} for _ in range(idx, 8)
+            {key: 0 for key in _TRIGGER_KEYS}
+            for _ in range(len(trigger_config) - 1, 8)
         ]
-        result.append(empty_stages)
+        trigger_config.append(empty_stages)
+    elif len(trigger_config) > 8:
+        trigger_config = trigger_config[:8]
 
-    return result
+    return trigger_config
 
 
 @validate_int_percent
