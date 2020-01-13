fb31b7b0d39c974e395fc742289bb150c1de6577
Bruno Morais <brunosmmm@gmail.com>
2020-01-13 13:23:46 -0500

    fix argument name clash

--- logicap/validators.py
+++ logicap/validators.py
@@ -6,27 +6,27 @@ from logicap.config import TestConfigurationError
 def validate_integer(fn):
     """Validate integer."""
 
-    def _validate(value, **kwargs):
+    def _validate(_value, **kwargs):
         """Perform validation."""
-        if not isinstance(value, int):
+        if not isinstance(_value, int):
             # try converting
             try:
-                value = int(value)
+                value = int(_value)
             except ValueError:
                 try:
-                    if value.startswith("0x"):
-                        value = value[2:]
-                    value = int(value, 16)
+                    if _value.startswith("0x"):
+                        _value = _value[2:]
+                    _value = int(_value, 16)
                 except ValueError:
                     try:
-                        if value.startswith("0b"):
-                            value = value[2:]
-                        value = int(value.lstrip("0b"), 2)
+                        if _value.startswith("0b"):
+                            _value = _value[2:]
+                        _value = int(_value.lstrip("0b"), 2)
                     except ValueError:
                         raise TestConfigurationError(
-                            f"cannot convert value to integer: '{value}'"
+                            f"cannot convert value to integer: '{_value}'"
                         )
-        return fn(value, **kwargs)
+        return fn(_value, **kwargs)
 
     return _validate
 
@@ -35,11 +35,11 @@ def validate_positive_integer(fn):
     """Validate positive integer."""
 
     @validate_integer
-    def _validate(value, **kwargs):
+    def _validate(_value, **kwargs):
         """Perform validation."""
-        if value < 0:
+        if _value < 0:
             raise TestConfigurationError("value must be a positive integer")
-        return fn(value, **kwargs)
+        return fn(_value, **kwargs)
 
     return _validate
 
@@ -47,11 +47,11 @@ def validate_positive_integer(fn):
 def validate_string(fn):
     """Validate string value."""
 
-    def _validate(value, **kwargs):
+    def _validate(_value, **kwargs):
         """Perform validation."""
-        if not isinstance(value, str):
+        if not isinstance(_value, str):
             raise TestConfigurationError("value must be a string")
 
-        return fn(value, **kwargs)
+        return fn(_value, **kwargs)
 
     return _validate
