67a28287c397939aeaf06f95ed7d83b998426f10
Bruno Morais <brunosmmm@gmail.com>
2020-01-13 13:34:59 -0500

    generic type validator

--- logicap/validators.py
+++ logicap/validators.py
@@ -3,6 +3,37 @@
 from logicap.config import TestConfigurationError
 
 
+class Validator:
+    """Validator abstract class."""
+
+
+class ValidateType(Validator):
+    """Type validator."""
+
+    def __init__(self, _type):
+        """Initialize."""
+        self._type = _type
+
+    @property
+    def target_type(self):
+        """Get target type."""
+        return self._type
+
+    def __call__(self, fn):
+        """Decorator."""
+
+        def _validate(_value, **kwargs):
+            """Perform validation."""
+            if not isinstance(_value, self.target_type):
+                raise TestConfigurationError(
+                    f"value must be of type '{self.target_type}'"
+                )
+
+            return fn(_value, **kwargs)
+
+        return _validate
+
+
 def validate_integer(fn):
     """Validate integer."""
 
@@ -47,11 +78,20 @@ def validate_positive_integer(fn):
 def validate_string(fn):
     """Validate string value."""
 
+    @ValidateType(str)
     def _validate(_value, **kwargs):
         """Perform validation."""
-        if not isinstance(_value, str):
-            raise TestConfigurationError("value must be a string")
+        return fn(_value, **kwargs)
 
+    return _validate
+
+
+def validate_list(fn):
+    """Validate lists."""
+
+    @ValidateType((tuple, list))
+    def _validate(_value, **kwargs):
+        """Perform validation"""
         return fn(_value, **kwargs)
 
     return _validate
