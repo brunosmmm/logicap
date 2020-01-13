51ecf141d83618858eda9c8de0e01fbb2d782636
Bruno Morais <brunosmmm@gmail.com>
2020-01-13 14:15:10 -0500

    use more decorator insanity

--- logicap/inputs.py
+++ logicap/inputs.py
@@ -1,10 +1,6 @@
 """Input generation."""
 
-from logicap.config import (
-    validate_config,
-    TestConfigurationError,
-    DeferValidation,
-)
+from logicap.config import validate_config
 
 from logicap.validators import (
     validate_positive_integer,
@@ -12,7 +8,7 @@ from logicap.validators import (
     ValidateChoice,
 )
 
-from logicap.util import AutoValidateList
+from logicap.util import AutoValidateList, KeyDependencyMap
 
 EVENT_TYPES = ("initial", "set", "clear", "toggle")
 _EVENT_DEPS = {
@@ -25,18 +21,10 @@ _EVENT_DEPS = {
 
 @validate_string
 @ValidateChoice(EVENT_TYPES)
+@KeyDependencyMap(**_EVENT_DEPS)
 def _validate_evt_type(evt_type, **kwargs):
     """Validate event type."""
-    evt_deps = _EVENT_DEPS[evt_type]
-    missing_deps = []
-    for dep in evt_deps:
-        if dep not in kwargs:
-            missing_deps.append(dep)
-
-    if missing_deps:
-        raise DeferValidation(*missing_deps)
-
-    # TODO fail if there are pending defers at end of validation
+    return evt_type
 
 
 @validate_positive_integer
