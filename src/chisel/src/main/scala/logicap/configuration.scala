025c833c5adcff2b1b811ac4fb757a4aeba6d216
Bruno Morais <brunosmmm@gmail.com>
2020-02-04 17:25:09 -0500

    add some more defaults

--- src/chisel/src/main/scala/logicap/configuration.scala
+++ src/chisel/src/main/scala/logicap/configuration.scala
@@ -3,4 +3,8 @@ package Logicap
 package object LogicapConfiguration {
   val LogicWidth: Int = 32
   val TriggerLevels: Int = 8
+  val MaxClkDiv: Int = 32
+  val MaxSampleCount: Int = 4*1024*1024 // 4 Mega samples
+  val AXIStreamWidth: Int = 64
+  val AXIStreamDefaultParams = new AXIStreamInterfaceParams(dataWidth=AXIStreamWidth, hasTlast=true)
 }
