c8a44be7b58725feb7bbb28633e2c7a0453deaec
Bruno Morais <brunosmmm@gmail.com>
2020-02-04 13:35:23 -0500

    fix Flipped usage

--- src/chisel/src/main/scala/logicap/axis_fifo.scala
+++ src/chisel/src/main/scala/logicap/axis_fifo.scala
@@ -8,7 +8,7 @@ class AXISFifo(dataWidth: Int, depth: Int) extends Module{
   val io = IO(
     new Bundle {
       val slave_if = IO(new AXIStreamIO(AXIStreamInterfaceParams(dataWidth = dataWidth, hasTlast = false)))
-      val master_if = Flipped(IO(new AXIStreamIO(AXIStreamInterfaceParams(dataWidth = dataWidth, hasTlast = true))))
+      val master_if = IO(Flipped(new AXIStreamIO(AXIStreamInterfaceParams(dataWidth = dataWidth, hasTlast = true))))
       val slave_clock = Input(Clock())
       val master_clock = Input(Clock())
     })
